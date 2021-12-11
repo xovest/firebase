91375f49c8c25e7574b7226c5d8a851972dc5e54
xovest <79008917+xovest@users.noreply.github.com>
2021-12-11 21:35:41 +0200

    login logout

--- src/index.js
+++ src/index.js
@@ -110,3 +110,32 @@ signupForm.addEventListener('submit', e => {
       console.log(err.message);
     })
 })
+
+
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
