6d032d7042ecfe3d88c4bde77522b4162f14dae9
xovest <79008917+xovest@users.noreply.github.com>
2021-12-11 16:36:51 +0200

    firebase config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('sup');
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: "fir-9-7fca0.firebaseapp.com",
+  projectId: "fir-9-7fca0",
+  storageBucket: "fir-9-7fca0.appspot.com",
+  messagingSenderId: "653073579695",
+  appId: "1:653073579695:web:1eb47754c9766ad9935e5a"
+}
+
+initializeApp(firebaseConfig)
