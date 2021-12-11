f4e248dfe63bf6aca4431088927908234015edd5
xovest <79008917+xovest@users.noreply.github.com>
2021-12-11 21:33:47 +0200

    sign

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ import {
   getDoc,
   updateDoc
 } from 'firebase/firestore'
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBiKVbZyG2Iqqq6vzmbb6gk9oCpk2diCyE",
@@ -23,14 +27,20 @@ const firebaseConfig = {
   appId: "1:653073579695:web:1eb47754c9766ad9935e5a"
 }
 
+//init firebase
 initializeApp(firebaseConfig)
 
+//init service
 const db = getFirestore()
+const auth = getAuth()
 
+//collection ref
 const colRef = collection(db, 'books')
 
+//queries
 const q = query(colRef, orderBy('createdAt'))
-  
+
+//real time collect
 onSnapshot(q, (snapshot) => {
   let books = []
   snapshot.docs.forEach(doc => {
@@ -81,5 +91,22 @@ deleteBookForm.addEventListener('submit', (e) => {
     })
     .then(() => {
       updateForm.reset()
+    })
+})
+
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user created:', cred.user);
+      signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message);
     })
 })
